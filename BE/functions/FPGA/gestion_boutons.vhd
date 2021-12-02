5b4e3a1e5797fdae8ef3a77a3c1b11383496de11
Mouloud ZIANE <zimouloud@outlook.fr>
2021-12-02 10:05:24 +0100

    dev: Modification de la machine à etat en prenant en compte les bips simples et doubles

--- BE/functions/FPGA/gestion_boutons.vhd
+++ BE/functions/FPGA/gestion_boutons.vhd
@@ -26,33 +26,34 @@ USE ieee.std_logic_arith.all;
 
 ENTITY gestion_boutton IS
 	PORT (
-			clk, chipselect, write_n, reset_n: in std_logic;
-			writedata : in std_logic_vector (31 downto 0);
-			address : in std_logic;
-			readdata : out std_logic_vector (31 downto 0);
-			BP_Babord,BP_Tribord, BP_STBY  : in std_logic;  
-			ledBabord, ledTribord,ledSTBY, out_bip : out std_logic		  
+			clk, reset_n: in std_logic;
+	        BP_Babord,BP_Tribord, BP_STBY  : in std_logic;  
+			--ledBabord, ledTribord,ledSTBY, out_bip : out std_logic
+			out_bip : out std_logic		  
 		  );
 END gestion_boutton  ;
 
 ARCHITECTURE arch_gestion_boutton OF gestion_boutton IS
 signal fin_tempo, val_tempo, val_bip, fin_bip, bip_simple, bip_double: std_logic;
 signal codeFonction: std_logic_vector (3 downto 0);
-signal clk_1Hz, bip, clk_50, clk_100, rst_n : std_logic;
-signal compt_bip: integer range 0 to 200;
+
+
 
 begin
 	--********************************************************************
 --state machine bouton poussoir
 --************************************************************************
-gestion_bp:process (raz_n, clk_100)
+gestion_bp:process (raz_n, clk)
+--Avec cette horloge le signal va tres vite, on ne peut  pas  voir tout
+--FIXEME
+--Idée c'est d'utiliser un génerateur d'impulsion tt les seconds, ce que genere un signal à 20 ns
 variable State : State_button;
 begin
 	if raz_n ='0' then
 	state:= s0;
 	codeFonction <="0000";
-	elsif clk_100'event and clk_100='1' then
-		case etat is 
+	elsif rising_edge(clk) then
+	case state is
 		when s0 =>
 		if BP_Babord='0' then 
 		state:=s1; codeFonction <="0001";
@@ -63,56 +64,52 @@ begin
 		if BP_STBY='0' then 
 		state:=s3; codeFonction <="0000"; 
 		end if;
-		ledSTBY <= clk_1Hz; ledBabord <= led_faible; ledTribord <= led_faible;
+		
 	when s1 =>
 		if BP_Babord='1' then 
 		state:=s0; codeFonction <="0000";
 		end if;
-		ledSTBY <= clk_1Hz; ledBabord <= led_faible; ledTribord <= '0';
 	when s2 =>
 		if BP_Tribord='1' then 
 		state:=s0; codeFonction <="0000";
 		end if;
-		ledSTBY <= clk_1Hz; ledBabord <= '0'; ledTribord <= led_faible;
+		
 	when s3 =>
 		if BP_STBY='1' then 
 		state:=s4; codeFonction <="0011";
 		end if;
-		ledSTBY <= clk_1Hz; ledBabord <= led_intense; ledTribord <= led_intense;
+		
 	when s4 =>
 		if BP_STBY='0' then 
-		state:=s5; codeFonction <="0000";
+		state:=s5; -- pas d'action
 		end if;
-		if BP_Babord='0' and BP_Tribord='1'then 
-		state:=s6; codeFonction <="0011"; val_tempo <='1';
+		if BP_Babord='0' then 
+		state:=s6; val_tempo <='1'; -- tempo actif 
 		end if;
-		if BP_Tribord='0' and BP_Babord='1'then 
-		state:=s9; codeFonction <="0011";val_tempo <='1';
+		if BP_Tribord='0' then 
+		state:=s9; val_tempo <='1' ;-- tempo actif 
 		end if;
-		ledSTBY <= '1'; ledBabord <= clk_50; ledTribord <= clk_50;
 	when s5 =>
-		if BP_STBY='1' then 
+	if BP_STBY='1' then 
 		state:=s0; codeFonction <="0000";
 		end if;
-		ledBabord <= clk_50; ledTribord <= clk_50;
 	when s6 =>
-		if BP_Babord='0' and fin_tempo='1' then 
+	if BP_Babord='0' and fin_tempo='1' then 
 		state:=s7; codeFonction <="0101";val_tempo <='0'; bip_double<='1';
 		end if;
-		if BP_Babord='1' and fin_tempo='0' then 
+	if BP_Babord='1' and fin_tempo='0' then 
 		state:=s8; codeFonction <="0100"; val_tempo <='0'; bip_simple<='1';
 		end if;
-		ledBabord <= '0'; ledTribord <= clk_50;
 	when s7 =>
 		if fin_bip='1' then 
-		state:=s13; codeFonction <="0101"; bip_double<='0';
+		state:=s4; codeFonction <="0011"; bip_double<='0';
 		end if;
-		ledBabord <= bip ; ledTribord <= clk_50;
 	when s8 =>
 		if fin_bip='1' then 
 		state:=s4; codeFonction <="0011";bip_simple<='0'; 
 		end if;
-		ledBabord <= bip ; ledTribord <= clk_50;
+	end case;
+
 	when s9 =>
 		if BP_Tribord='0' and fin_tempo='1' then 
 		state:=s10; codeFonction <="0110"; val_tempo <='0'; bip_double<='1';
@@ -120,27 +117,15 @@ begin
 		if BP_Tribord='1' and fin_tempo='0' then 
 		state:=s11; codeFonction <="0111"; val_tempo <='0'; bip_simple<='1';
 		end if;
-		ledBabord <= clk_50; ledTribord <= '0';
+
 	when s10 =>
 		if fin_bip='1' then 
-		state:=s12; codeFonction <="0110"; bip_double<='0';
+		state:=s4; codeFonction <="0011"; bip_double<='0';
 		end if;
-		ledBabord <= clk_50; ledTribord <= bip;
+
 	when s11 =>
 		if fin_bip='1' then 
 		state:=s4; codeFonction <="0011"; bip_simple<='0';
 		end if;
-		ledBabord <= clk_50; ledTribord <= bip;
-	when s12 =>
-		if BP_Tribord='1'  then 
-		state:=s4; codeFonction <="0011"; bip_double<='0';
-		end if;
-		ledBabord <= clk_50; ledTribord <= clk_50;
-	when s13 =>
-		if BP_Babord='1'  then 
-		state:=s4; codeFonction <="0011"; bip_double<='0';
-		end if;
-		ledBabord <= clk_50; ledTribord <= clk_50;
-	end case;
 	end if;
 	end process gestion_bp;
